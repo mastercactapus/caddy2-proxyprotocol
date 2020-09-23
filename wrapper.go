3a92c8137aa1c283ffd5911f9befb2e0f60b84b5
Nathaniel Caza <mastercactapus@gmail.com>
2020-09-23 17:35:37 -0500

    document Wrapper

--- wrapper.go
+++ wrapper.go
@@ -9,6 +9,7 @@ import (
 	"github.com/mastercactapus/proxyprotocol"
 )
 
+// Wrapper provides PROXY protocol support to Caddy by implementing the caddy.ListenerWrapper interface. It must be loaded before the `tls` listener.
 type Wrapper struct {
 	// Timeout specifies an optional maximum time for the PROXY header to be received. If zero, timeout is disabled. Default is 5s.
 	Timeout caddy.Duration `json:"timeout,omitempty"`
